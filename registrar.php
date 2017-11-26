cecaf627ae93a4a996e6b38c0e60d2a6633208ba
rprieto35654 <33473429+rprieto35654@users.noreply.github.com>
2017-11-25 21:37:40 -0500

    Modificada pagina de Login, creada pagina registro y script de conexion.

---
+++ registrar.php
@@ -0,0 +1,126 @@
+<?php
+
+require_once 'config.php';
+
+
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Por favor ingrese un nombre de usuario.";
+    } else{
+
+        $sql = "SELECT id FROM users WHERE username = ?";
+
+        if($stmt = mysqli_prepare($link, $sql)){
+
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+
+            $param_username = trim($_POST["username"]);
+
+
+            if(mysqli_stmt_execute($stmt)){
+
+                mysqli_stmt_store_result($stmt);
+
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "Este nombre de usuario ya existe.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Algo ha salido mal por favor intente mas tarde.";
+            }
+        }
+
+
+        mysqli_stmt_close($stmt);
+    }
+
+
+    if(empty(trim($_POST['password']))){
+        $password_err = "Por favor ingrese una contraseña.";
+    } elseif(strlen(trim($_POST['password'])) < 6){
+        $password_err = "La contraseña debe componerse de minimo 6 caracteres.";
+    } else{
+        $password = trim($_POST['password']);
+    }
+
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = 'Por favor confirme la contraseña.';
+    } else{
+        $confirm_password = trim($_POST['confirm_password']);
+        if($password != $confirm_password){
+            $confirm_password_err = 'Las contraseñas no coinciden.';
+        }
+    }
+
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+            if(mysqli_stmt_execute($stmt)){
+                header("location: login.php");
+            } else{
+                echo "Algo ha salido mal por favor intente mas tarde.";
+            }
+        }
+
+        mysqli_stmt_close($stmt);
+    }
+
+    mysqli_close($link);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Registrar</h2>
+        <p>Por favor llene el siguiente formulario para crear una cuenta.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username:<sup>*</sup></label>
+                <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password:<sup>*</sup></label>
+                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>Confirm Password:<sup>*</sup></label>
+                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-default" value="Reset">
+            </div>
+            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
